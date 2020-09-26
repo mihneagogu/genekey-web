107c0772fc525dc5d2fcec10f1272177bb2676bc
mihneagogu <gogumihnea@gmail.com>
2020-09-26 22:08:34 +0300

    added the libraries

--- scripts/genekeys.ts
+++ scripts/genekeys.ts
@@ -1,10 +1,37 @@
 // Model genekey for index 0
-let gkModel =[{"index":0,"codone":21,"shadow":{"type":"SHADOW","description":"shadow"},"gift":{"type":"GIFT","description":"gift"},"siddhi":{"type":"SIDDHI","description":"siddhi"},"organs":["plamani","inima","rinichi"], "emotions": ["frica", "semne"], "partner": 45, "dilemma": "Eternal dilemma", "aminoacid": "valine", "iching": "some iching", "keywords": ["virtue", "dignity"], "channel": {"keys": [0, 64], "name": "Some channel"}}];
-let gk0 = new GeneKey(gkModel[0]);
+const gkModel =[{"index":0,"codone":21,"shadow":{"type":"SHADOW","description":"shadow"},"gift":{"type":"GIFT","description":"gift"},"siddhi":{"type":"SIDDHI","description":"siddhi"},"organs":["plamani","inima","rinichi"], "emotions": ["frica", "semne"], "partner": 45, "dilemma": "Eternal dilemma", "aminoacid": "valine", "iching": "some iching", "keywords": ["virtue", "dignity"], "channel": {"keys": [0, 64], "name": "Some channel"}}];
+const gk0 = new GeneKey(gkModel[0]);
 
-let geneKeyLibrary: GeneKey[] = [gk0];
+
+const geneKeyLibrary: GeneKey[] = [gk0];
+const dilemmaLibrary: string[] = [];
+const aminoacidLibrary = {};
+const organLibrary = {};
+const keywordLibrary = {};
 // Push an array of 64 mock genekeys
 for (let i = 1; i <= GKConstants.MaxKey; i++) {
-    geneKeyLibrary.push(gk0.deeplConeId(i));
+    const gk: GeneKey = gk0.deeplConeId(i);
+    geneKeyLibrary.push(gk);
+    dilemmaLibrary.push(`Dilemma: ${gk.dilemma} for GK ${gk.index}`);
+    addToLibrary(aminoacidLibrary, gk.aminoacid, gk.index);
+    gk.organs.forEach(org => addToLibrary(organLibrary, org, gk.index));
+    gk.keywords.forEach(kw => addToLibrary(keywordLibrary, kw, gk.index));
 }
+
+function addToLibrary(library, property, toAdd) {
+    const keys = library[property];
+    if (!keys) {
+        library[property] = [toAdd];
+    } else {
+        keys.push(toAdd);
+    }
+}
+
+function printLibrary(lib) {
+    console.log(lib);
+}
+
+printLibrary(aminoacidLibrary);
+printLibrary(organLibrary);
+printLibrary(keywordLibrary);
 
